9e44938d6fb79e9280a113d24b292660fb7d4e18
Ryan Diaz <email@ryananthonydiaz.com>
2020-07-22 15:51:45 -0700

    dimension state brought into app

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import CheckerBoard from './components/checkerBoard/CheckerBoard';
 import styles from './App.module.css';
 
 function App() {
-  const { DimensionProvider } = useDimensionStore();
+  const { DimensionProvider, useDimension } = useDimensionStore();
+  const { dimension } = useDimension();
+
   const board = [];
-  for (let row = 1; row <= 8; row++) {
+  for (let row = 1; row <= dimension; row++) {
     board.push(<CheckerBoard even={ row % 2 === 0 ? false : true} />)
   }
   return (
