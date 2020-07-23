ec5c602eb7a7e616eb53b82d9731cc27e6aad758
Ryan Diaz <email@ryananthonydiaz.com>
2020-07-22 17:44:43 -0700

    commit before leaving

--- src/state/useGameStore.js
+++ src/state/useGameStore.js
@@ -3,6 +3,7 @@ import makeStore from './makeStore';
 function buildBrandNewGame(size) {
   const checkerBoard = {}
   let checkerBoardRow;
+
   for (let row = 1; row <= size; row++) {
     checkerBoardRow = [];
     for(let col = 1; col <= size; col++) {
@@ -30,7 +31,7 @@ const gameReducer = (state = initialState, action) => {
       return {
         ...state,
         dimension: action.dimension,
-      }
+      };
     default:
       return state;
   }
